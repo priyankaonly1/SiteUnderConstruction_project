000885ff3aaa84724a17511d67244306cf7731a7
Priyanka Biswas <priyankabiswasonly1@gmail.com>
2021-07-09 16:09:53 +0530

    first_commit

---
+++ mysite/middlewares.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+class UnderConstructionMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        print('Call From Middleware Before View')
+        response = render(request, 'mysite/siteUC.html')
+
+        print('Call From Middleware After View')
+        return response
